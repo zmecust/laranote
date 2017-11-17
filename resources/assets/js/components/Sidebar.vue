ff06568084af6308b80ea1236980628f79919feb
zhangmin <zhangmin@inesatech.local>
2017-11-17 18:00:52 +0800

    add sth

--- resources/assets/js/components/Sidebar.vue
+++ resources/assets/js/components/Sidebar.vue
@@ -1,49 +1,40 @@
 <template>
-<el-row class="tac">
-  <el-col :span="24">
-    <h3>菜单</h3>
-    <el-menu
-      default-active="1"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
-      background-color="#f4f4f4"
-      text-color="#555"
-      active-text-color="#20c997">
+  <el-row class="tac">
+    <el-col :span="24">
+      <h4>菜单</h4>
+      <el-menu mode="vertical" default-openeds="['1', '2', '3']" :default-active="router" class="el-menu-vertical-demo" theme="light" unique-opened>
 
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-tickets"></i>
-          <span>笔记本</span>
-        </template>
-        <el-submenu :index="category.id" v-for="category in categories" :key="category.id">
-          <template slot="title">{{ category.name }} (共 {{ category.notes.length }} 篇)</template>
-          <el-menu-item :index="note.id" v-for="note in category.notes" :key="note.id">
-            <a :href="note.id">
-            {{ note.title }} {{ note.created_at }}
-            </a>
-          </el-menu-item>
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-tickets"></i>
+            <span>笔记本</span>
+          </template>
+          <el-submenu :index="category.name" v-for="category in categories" :key="category.id">
+            <template slot="title">{{ category.name }} (共 {{ category.notes.length }} 篇)</template>
+            <el-menu-item :index="'/notes/' + note.id" v-for="note in category.notes" :key="note.id">
+              {{ note.title }} {{ note.created_at }}
+            </el-menu-item>
+          </el-submenu>
         </el-submenu>
-      </el-submenu>
-      
-      <el-menu-item index="2">
-        <i class="el-icon-delete"></i>
-        <span slot="title">垃圾桶</span>
-      </el-menu-item>
 
-      <el-menu-item index="3">
-        <i class="el-icon-star-on"></i>
-        <span slot="title">我的收藏</span>
-      </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-delete"></i>
+          <span slot="title">垃圾桶</span>
+        </el-menu-item>
 
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">设置</span>
-      </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-star-on"></i>
+          <span slot="title">我的收藏</span>
+        </el-menu-item>
 
-    </el-menu>
-  </el-col>
-</el-row>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">设置</span>
+        </el-menu-item>
+
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -51,23 +42,35 @@
 export default {
   data() {
     return {
-      categories: ''
+      categories: '',
+    }
+  },
+  computed: {
+    router() {
+      return this.GetUrlRelativePath();
     }
   },
   mounted() {
     this.$http.get('/categories').then(res => {
       if (res.data.status) {
         this.categories = res.data.data;
-        console.log(this.categories);
       }
     });
+    console.log(this.router);
   },
   methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+    handleSelect(index) {
+      console.log(index);
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    GetUrlRelativePath() {
+      var url = document.location.toString();
+      var arrUrl = url.split("//");
+      var start = arrUrl[1].indexOf("/");
+      var relUrl = arrUrl[1].substring(start);
+      if (relUrl.indexOf("?") != -1) {
+        relUrl = relUrl.split("?")[0];
+      }
+      return relUrl;
     }
   }
 }
